68fe93add904c3a457593b4fbf3da986fd672a6e
ErikVilar <erikvilaralves@gmail.com>
2025-04-07 13:49:20 -0300

    main project

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Main from "./components/main";
 import "./components/styles/app.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
+/*
+ *Principal component
+ */
 function App() {
 
   return <Main />;
